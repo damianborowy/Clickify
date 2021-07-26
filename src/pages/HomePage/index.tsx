4c8ebb78270345c68665540d767d5f8f2124f2b4
Damian Borowy <33089881+damianborowy@users.noreply.github.com>
2021-07-26 18:09:26 +0200

    add some example vertical marquee

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,13 +1,30 @@
+import Marquee from "react-marquee-master";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { docco } from "react-syntax-highlighter/dist/esm/styles/hljs";
 import { useRecoilState } from "recoil";
 import { fooAtom } from "../../store/foo";
+
+const codeString = `
+const foo = 5;
+
+function aaa(a, b, c) {
+  return a + b + c + foo;
+}
+`;
 
 const HomePage = () => {
     const [foo, setFoo] = useRecoilState(fooAtom);
 
     return (
-      <div>
-        <button onClick={() => setFoo(foo + 1)}>{foo}</button>
-      </div>
+      <Marquee
+        marqueeItems={[...new Array(5)].map(() => (
+          <SyntaxHighlighter language="javascript" style={docco}>
+            {codeString}
+          </SyntaxHighlighter>
+        ))}
+        height={300}
+        delay={30}
+      />
     );
 };
 
